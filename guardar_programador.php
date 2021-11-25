c1929a75af8bafc369bc060fc43fec191ffa043e
Edwin nieto <egnieto94@gmail.com>
2021-11-25 18:34:01 -0500

    correccion en sentencia sql

--- guardar_programador.php
+++ guardar_programador.php
@@ -4,7 +4,7 @@
 if(!empty($_POST)){
 	include "connection.php";
 	$con  = connect();
-	$sql = "insert into programador (nombre,apellido,cedula,correo,lenguajes,fecha_creacion) value (\"".$_POST["nombre"]."\",\"".$_POST["apellido"]."\",\"".$_POST["cedula"]."\",\"".$_POST["correo"]."\",\"".$_POST["correo"]."\",NOW())";
+	$sql = "insert into programador (nombre,apellido,cedula,correo,lenguajes,fecha_creacion) value (\"".$_POST["nombre"]."\",\"".$_POST["apellido"]."\",\"".$_POST["cedula"]."\",\"".$_POST["correo"]."\",\"".$_POST["lenguajes"]."\",NOW())";
 	$con->query($sql);
 	header("Location: datos.php");
 }
